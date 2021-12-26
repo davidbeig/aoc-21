8033cd109cce9d65f5b96be5d4502384c058f720
David Igón <david.igon@riseup.net>
2021-12-26 23:26:49 +0100

    Day 6. Part 2.

--- 06/main.js
+++ 06/main.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 
 class LanternFish {
+  static REPRODUCTION_CYCLE = 7
+  static NEW_BORN_CYCLE = 8
+  #cacheDescendants = {}
+
   constructor (school, days) {
     this.school = school
     this.days = days
@@ -12,16 +16,49 @@ class LanternFish {
       for (let j = 0; j < length; j++) {
         this.school[j]--
         if (parseInt(this.school[j]) < 0) {
-          this.school.push(8)
-          this.school[j] = 6
+          this.school.push(LanternFish.NEW_BORN_CYCLE)
+          this.school[j] = LanternFish.REPRODUCTION_CYCLE - 1
         }
       }
     }
 
     return this.school.length
   }
+
+  calculate() {
+    let totalLanternFishes = this.school.length
+
+    this.school.forEach(element => {
+      totalLanternFishes += this.#calculateDescendants(this.days, parseInt(element))
+    })
+
+    return totalLanternFishes
+  }
+
+  #calculateDescendants(daysLeft, reproductionDays) {
+    if (reproductionDays >= daysLeft) return 0
+    if ( (daysLeft - reproductionDays) < LanternFish.REPRODUCTION_CYCLE) return 1
+    if (daysLeft in this.#cacheDescendants)
+      return this.#cacheDescendants[daysLeft]
+
+    let descendants = Math.floor( (daysLeft - reproductionDays - 1) / LanternFish.REPRODUCTION_CYCLE) + 1
+    let acc = descendants
+    for(let i = 0; i < descendants; i++) {
+      let newBornDaysLeft = (daysLeft - reproductionDays) - (i * LanternFish.REPRODUCTION_CYCLE) - 1
+      const descendants = this.#calculateDescendants(newBornDaysLeft , LanternFish.NEW_BORN_CYCLE)
+
+      if (!newBornDaysLeft in this.#cacheDescendants) this.#cacheDescendants[newBornDaysLeft] = {}
+      this.#cacheDescendants[newBornDaysLeft] = descendants
+      acc += descendants
+    }
+
+    return acc
+  }
 }
 
 const school = fs.readFileSync('input').toString().split(',')
-const lanternFish = new LanternFish(school, 80)
+let lanternFish = new LanternFish([...school], 80)
 console.info(lanternFish.simulate())
+
+lanternFish = new LanternFish(school, 256)
+console.info(lanternFish.calculate())
