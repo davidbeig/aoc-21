359bedf8d62dee72fa57a61669e4b6158303bb24
David Igón <david.igon@riseup.net>
2021-12-27 20:52:41 +0100

    Day 3. Part 2.

--- 03/main.js
+++ 03/main.js
@@ -5,46 +5,64 @@ class Diagnostic {
   #diagnostic 
   #epsilonRate
   #gammaRate
+  #co2Scrubber
+  #oxigenGenerator
 
   constructor (diagnostic) {
     this.#diagnostic = diagnostic
     this.#epsilonRate = this.#gammaRate = 0
+    this.#co2Scrubber = this.#oxigenGenerator = []
     this.process()
   }
 
   process () {
-    
+    let mostCommonDiagnostic = [...this.#diagnostic]
+    let lessCommonDiagnostic = [...this.#diagnostic]
     for (let i = 0; i < this.#diagnostic[0].length; i++) {
-      this.#epsilonRate = (this.#epsilonRate << 1) + this.#processColumnByMostCommon(i)
-      this.#gammaRate = (this.#gammaRate << 1) + !this.#processColumnByMostCommon(i)
+      if (mostCommonDiagnostic.length != 1) mostCommonDiagnostic = this.#processColumnByMostCommon(mostCommonDiagnostic, i)
+      if (lessCommonDiagnostic.length != 1) lessCommonDiagnostic = this.#processColumnByLessCommon(lessCommonDiagnostic, i)
+
+      this.#epsilonRate = (this.#epsilonRate << 1) + this.#findMostCommon(this.#diagnostic, i)
+      this.#gammaRate = (this.#gammaRate << 1) + !this.#findMostCommon(this.#diagnostic, i)
     }
+
+    this.#oxigenGenerator = parseInt(mostCommonDiagnostic[0], 2)
+    this.#co2Scrubber = parseInt(lessCommonDiagnostic[0], 2)
   }
 
-  #processColumnByMostCommon(position) {
-    const mostCommon = this.#findMostCommon(position)
-    return mostCommon
+  #processColumnByMostCommon(array, position) {
+    const mostCommon = this.#findMostCommon(array, position)
+    array = array.filter( line => line[position] == mostCommon)
+    return array
   }
 
-  #processColumnByLessCommon(position) {
-    const lessCommon = !this.#findMostCommon(position)
-    return lessCommon
+  #processColumnByLessCommon(array, position) {
+    const lessCommon = !this.#findMostCommon(array, position)
+    array = array.filter( line => line[position] == lessCommon)
+    return array
   }
 
-  #findMostCommon(position) {
+  #findMostCommon(array, position) {
     let countOfOnes = 0
-    for (let i = 0; i < this.#diagnostic.length; i++) {
-      countOfOnes += this.#diagnostic[i][position] == 1
+    for (let i = 0; i < array.length; i++) {
+      countOfOnes += array[i][position] == 1
     }
     
-    return countOfOnes >= (this.#diagnostic.length / 2 )
+    return countOfOnes >= (array.length / 2 )
   }
 
   powerConsumption () {
     return this.#gammaRate * this.#epsilonRate
+  }
+
+  lifeSupport () {
+    return this.#oxigenGenerator * this.#co2Scrubber
   }
 }
 
 const input = fs.readFileSync('input').toString().split('\n')
 const diagnostic = new Diagnostic(input)
 const powerConsumption = diagnostic.powerConsumption()
+const lifeSupport = diagnostic.lifeSupport()
 console.info(powerConsumption)
+console.info(lifeSupport)
